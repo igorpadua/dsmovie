1d3cc5bfca7726742085630dd789c2569645a6ae
igorpadua <igorpadua@tutanota.com>
2022-03-23 13:35:26 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import {BASE_URL} from "utils/requests";
 
 function Listing() {
+
+	axios.get(`${BASE_URL}/movies?size==12&page=0`)
+		.then(response => { 
+			console.log(response.data);
+		});
+
 	return (
 		<>
 			<Pagination/>
@@ -33,7 +41,6 @@ function Listing() {
 
 			</div>
 		</>
-	)
-}
+	) }
 
 export default Listing;
